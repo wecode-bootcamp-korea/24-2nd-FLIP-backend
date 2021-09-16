4f967245cbdb599502c362a9871c20a87213b7e5
Jinatra <jinatra@Jinatraui-MacBookAir.local>
2021-09-16 11:52:05 +0900

    Edited: FK name revision

--- carts/models.py
+++ carts/models.py
@@ -5,8 +5,8 @@ from products.models import Product
 from users.models    import User
 
 class Cart(TimeStampModel):
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user_id    = models.ForeignKey(User, on_delete=models.CASCADE)
+    product    = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity   = models.IntegerField()
 
     class Meta:
